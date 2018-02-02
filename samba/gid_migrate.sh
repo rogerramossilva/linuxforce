580370573876eb992d37aa00ce4858d186fc722b
root <root@ad.asf.com>
2018-02-02 14:31:45 -0200

    2018020202

--- samba/gid_migrate.sh
+++ samba/gid_migrate.sh
@@ -6,25 +6,24 @@ smb_groups=('infrastructure' 'security' 'public' 'manager' 'marketing' 'owner')
 for grupos in ${smb_groups[@]}
     do 
         # Variavel com listagem de SIDS de usarios do AD
-        XID=`wbinfo --group-info=$grupos | cut -d: -f3`
-	echo $XID
-#
-#        # Variavel com UID de usuarios da base Unix
-#        ID="$(getent passwd $LSUSER | awk -F: '{print $3}')"
-#        
-#	# Converte SID para UID nos usuarios do AD
-#        wbinfo -S $SID >> /dev/null
-#		
+        xid=`wbinfo --group-info=$grupos | cut -d: -f3`
+
+	# Converte XID para GID nos usuarios do AD
+        sid=`wbinfo -G $xid`
+        
+	# Variavel com UID de usuarios da base Unix
+        gid=`getent group $grupos | cut -d: -f3`
+		
 	# Cria arquivo ldif temporario
- #       > /tmp/modify.ldif
+        > /tmp/modify.ldif
         
 	# Preenche o arquivo temporario com os novos dados
-#	echo "dn: CN=$SID" >> /tmp/modify.ldif
- #       echo "changetype: modify" >> /tmp/modify.ldif
- #       echo "replace: xidNumber" >> /tmp/modify.ldif
- #       echo "xidNumber: $ID" >> /tmp/modify.ldif
- #       echo "" >> /tmp/modify.ldif
+	echo "dn: CN=$sid" >> /tmp/modify.ldif
+        echo "changetype: modify" >> /tmp/modify.ldif
+        echo "replace: xidNumber" >> /tmp/modify.ldif
+        echo "xidNumber: $gid" >> /tmp/modify.ldif
+        echo "" >> /tmp/modify.ldif
 
         # Modificacao do mapeamento de UIDS via modify.ldif
-       # ldbmodify -H /var/lib/samba/private/idmap.ldb /tmp/modify.ldif
+        ldbmodify -H /var/lib/samba/private/idmap.ldb /tmp/modify.ldif
 done
