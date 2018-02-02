e7e3ea8ac30516128dbdb5f5cee95d7d1e6eacb7
root <root@ad.asf.com>
2018-02-02 16:39:20 -0200

    2018020204

---
+++ samba/make_dumpsters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+list=`samba-tool user list | egrep -v 'Administrator|krbtgt|Guest'`
+
+for users in $list
+    do
+        mkdir -p /srv/asf/samba/lixeiras/$users
+	chown $users:$users /srv/asf/samba/lixeiras/$users
+	chmod 700 /srv/asf/samba/lixeiras/$users
+done
