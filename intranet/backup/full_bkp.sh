d12ead7d76408ef96e933f942c0afae39f2f81d7
root <root@intranet.asf.com>
2018-02-12 00:20:04 -0200

    2018021201

--- intranet/backup/full_bkp.sh
+++ intranet/backup/full_bkp.sh
@@ -7,7 +7,13 @@ asf='/var/www/asf.com/public_html'
 logs='/var/log/apache2'
 express='/srv/www/express'
 
-xfsdump -l 0 -L backup - $asf | gzip | ssh -p 52030 root@storage dd of=$rdir/asf/asf-$data.0.dump.gz
-xfsdump -l 0 -L backup - $logs | gzip | ssh -p 52030 root@storage dd of=$rdir/logs/logs-$data.0.dump.gz
-xfsdump -l 0 -L backup - $express | gzip | ssh -p 52030 root@storage dd of=$rdir/express/express-$data.0.dump.gz
+dump=`which xfsdump`
+ssh=`which ssh`
+dd=`which dd`
+gz=`which gzip`
+
+
+$dump -l 0 -L backup - $asf | $gz | $ssh -p 52030 root@storage $dd of=$rdir/asf/asf-$data.0.dump.gz
+$dump -l 0 -L backup - $logs | $gz | $ssh -p 52030 root@storage $dd of=$rdir/logs/logs-$data.0.dump.gz
+$dump -l 0 -L backup - $express | $gz | $ssh -p 52030 root@storage $dd of=$rdir/express/express-$data.0.dump.gz
 
