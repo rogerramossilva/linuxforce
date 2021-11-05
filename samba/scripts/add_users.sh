38ee2c9a68abdaf6b9cc987e51b5dc0bd64829ec
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2021-11-05 20:04:17 -0300

    Update add_users.sh

--- samba/scripts/add_users.sh
+++ samba/scripts/add_users.sh
@@ -4,5 +4,5 @@ list="$(getent passwd | awk -F: '$3 >= 1000 {print $1}' | grep -v nobody)"
 
 for user in $list
     do
-        samba-tool user add $user 123Mudar --home-drive=H: --script-path=logon.vbs --home-directory=\\\\ad\\$user 
+        samba-tool user create $user 123Mudar --home-drive=H: --script-path=logon.vbs --home-directory=\\\\ad\\$user 
 done
