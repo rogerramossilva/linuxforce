92054193dce1bc4f9ede596b2e4b8a414d0871e7
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2021-11-05 20:59:45 -0300

    Update make_dumpsters.sh

--- samba/scripts/make_dumpsters.sh
+++ samba/scripts/make_dumpsters.sh
@@ -6,5 +6,5 @@ for users in $list
     do
         mkdir -p /srv/asf/samba/lixeiras/$users
 	chown $users:$users /srv/asf/samba/lixeiras/$users
-	chmod 700 /srv/asf/samba/lixeiras/$users
+	chmod 777 /srv/asf/samba/lixeiras/$users
 done
