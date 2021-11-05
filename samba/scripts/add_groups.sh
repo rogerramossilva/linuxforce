2c3a15cf2dc9786058e2ff3d550b767f4058dd35
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2021-11-05 20:42:16 -0300

    Update add_groups.sh

--- samba/scripts/add_groups.sh
+++ samba/scripts/add_groups.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Array com grupos de sistema
-all_groups=('infrastructure' 'security' 'public' 'manager' 'marketing' 'owner')
+all_groups=('infrastructure' 'security' 'public' 'manager' 'marketing' 'owner' 'devel')
 
 for group in ${all_groups[@]}
     do
@@ -18,6 +18,13 @@ for group in ${all_groups[@]}
                 samba-tool group addmembers $group gideon.goddard
                 samba-tool group addmembers $group elliot.alderson
                 samba-tool group addmembers $group lloyd.chung
+            ;;
+	    devel)
+                samba-tool group addmembers $group analista
+                samba-tool group addmembers $group gideon.goddard
+                samba-tool group addmembers $group elliot.alderson
+                samba-tool group addmembers $group lloyd.chung
+                samba-tool group addmembers $group webmaster
             ;;
             public)
                 samba-tool group addmembers $group analista
