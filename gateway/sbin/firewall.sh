101a2a2ce2ca09f7084dea6a2282be77b6fa0701
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2022-12-04 14:36:53 -0300

    Update firewall.sh

--- gateway/sbin/firewall.sh
+++ gateway/sbin/firewall.sh
@@ -36,62 +36,15 @@ iptables -P FORWARD DROP
 iptables -t nat -F
 iptables -t filter -F
 
-# Limpa a chain drop-it, se ela existir
-if [ -n "`iptables -L | grep drop-it`" ]
-    then
-        iptables -F drop-it
-fi
-
-# Limpa a chain syn-flood, se ela existir
-if [ -n "`iptables -L | grep syn-flood`" ]
-    then
-        iptables -F syn-flood
-fi
-
-# Limpa a chain ndrop-it, se ela existir
-if [ -n "`iptables -L | grep ndrop-it`" ]
-    then
-        iptables -F ndrop-it
-fi
-
 # Limpa as chains de usuarios
 iptables -X
 
 # Zera os contadores do iptables
 iptables -Z
 
-# Cria DROP chain
-iptables -N drop-it
-#iptables -A drop-it -j LOG --log-level info
-iptables -A drop-it -j REJECT
-
-# Cria a SYN-FLOOD chain
-iptables -N syn-flood
-
-# Habilita a protecao syn-flood
-iptables -A syn-flood -p tcp --syn -m limit --limit 1/s -j RETURN
-
-# Habilita a proteção contra PORT SCANNERS
-iptables -A syn-flood -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j RETURN
-
-# Habilita a proteção contra o ping da morte
-iptables -A syn-flood -p icmp --icmp-type echo-request -m limit --limit 1/s -j RETURN
-iptables -A syn-flood -j DROP
-
-# Cria NDROP chain
-iptables -N ndrop-it
-
-#iptables -A ndrop-it -j LOG --log-level info
-iptables -A ndrop-it -j REJECT
-
 #####################
 ### INPUT SESSION ###
 #####################
-
-# 0 - Aceita somente pacotes de redes validas
-iptables -A INPUT -i enp0s3 -s 192.168.0.0/16 -j drop-it
-iptables -A INPUT -i enp0s3 -s 172.16.0.0/12 -j drop-it
-iptables -A INPUT -i enp0s3 -s 10.0.0.0/8 -j drop-it
 
 # 1 - Habilita o loopback
 iptables -A INPUT -i lo -j ACCEPT
@@ -112,7 +65,7 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 #iptables -A INPUT -p udp -i enp0s9 -s $EXT -d $FWL1 --dport 123 -j ACCEPT
 
 # 6 - Habilita o INPUT vindo de DNSs para o gateway
-#iptables -A INPUT -i enp0s3 -d $DIP -p udp --sport 53 -j ACCEPT
+#iptables -A INPUT -p udp --sport 53 -j ACCEPT
 
 # 7 - Habilita o INPUT para o proxy. OBS: Desabilitar FORWARD para 80 e 443
 #iptables -A INPUT -p tcp -i enp0s8 -s $LAN -d $GTW --dport 3128 -j ACCEPT
@@ -120,23 +73,13 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 # 8 - Habilita o INPUT para VPN vindo do cliente Externo
 #iptables -A INPUT -p udp -i enp0s9 -s $EXT -d $FWL1 --dport 1194 -j ACCEPT
 
-# 9 - Permite acesso a aplicacao interna via VPN
-#iptables -A INPUT -p tcp -i tun0 -s $CVP -d $SVP -m multiport --dports 80,443 -j ACCEPT 
-
-# 10 - Permite requisições DHCP porta 67
+# 9 - Permite requisições DHCP porta 67
 iptables -A INPUT -p udp --dport 67 -j ACCEPT
 
-# 11 - Descomentar para permitir log de descarte
-#iptables -A INPUT -j drop-it
 
 ######################
 ### OUTPUT SESSION ###
 ######################
-
-# 0 - Envia pacotes somente a redes validas
-iptables -A OUTPUT -o enp0s3 -d 192.168.0.0/16 -j drop-it
-iptables -A OUTPUT -o enp0s3 -d 172.16.0.0/12 -j drop-it
-iptables -A OUTPUT -o enp0s3 -d 10.0.0.0/8 -j drop-it
 
 # 1 - Habilita o loopback
 iptables -A OUTPUT -o lo -j ACCEPT
@@ -151,9 +94,7 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 
 # 5 - Habilita a resolucao de nomes 
-iptables -A OUTPUT -o enp0s3 -p udp -d 8.8.8.8 --dport 53 -j ACCEPT
-#iptables -A OUTPUT -p udp -d $INT --dport 53 -j ACCEPT
-#iptables -A OUTPUT -p udp -d $INT --dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 # 6 - Habilita o OUTPUT para HTTP e HTTPS
 iptables -A OUTPUT -p tcp -o enp0s3 -s $DIP -m multiport --dports 80,443 -j ACCEPT
@@ -164,8 +105,9 @@ iptables -A OUTPUT -p tcp -o enp0s3 -s $DIP -m multiport --dports 80,443 -j ACCE
 # 8 - Habilita o envio de logs para o node Storage
 #iptables -A OUTPUT -p udp -o enp0s8 -s $GTW -d $STG --dport 514 -j ACCEPT
 
-# 9 - Descomentar para permitir log de descarte
-#iptables -A OUTPUT -j drop-it
+# 9 - SSH Cliente externo
+#iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+
 
 ###############
 ### FORWARD ###
@@ -199,7 +141,7 @@ iptables -A FORWARD -p tcp -s $LAN -o enp0s3 -m multiport --dports 20,21 -j ACCE
 #iptables -A FORWARD -p tcp -i enp0s9 -s $LNK -d $DTC -m multiport --dports 25,110,143,465,587,993,995 -j ACCEPT
 
 # 9 - Habilita o FORWARD para acesso ao WWW
-#iptables -A FORWARD -p tcp -i enp0s9 -s $LNK -d $INT -m multiport --dports 80,443 -j ACCEPT
+#iptables -A FORWARD -p tcp -i enp0s9 -s $LNK -d $STG -m multiport --dports 80,443 -j ACCEPT
 
 # 10 - Habilita o FORWARD para autenticacao externa no LDAP
 #iptables -A FORWARD -p tcp -i enp0s9 -s $LNK -d $DTC --dport 389 -j ACCEPT
@@ -207,8 +149,6 @@ iptables -A FORWARD -p tcp -s $LAN -o enp0s3 -m multiport --dports 20,21 -j ACCE
 #ssh PORT 52010 52020 52030 52040 52100
 iptables -A FORWARD -p tcp -m multiport --dports 52010,52020,52030,52040,52100 -j ACCEPT
 
-# 11 - Descomentar para permitir log de descarte
-#iptables -A FORWARD -j drop-it
 
 ###################
 ### NAT SESSION ###
@@ -227,7 +167,7 @@ iptables -t nat -A POSTROUTING -s $LAN -o enp0s3 -j MASQUERADE
 # 4 - Habilita acesso do cliente Externo a aplicacao
 #for WEB in 80 443
 #    do
-#        iptables -t nat -A PREROUTING -p tcp -i enp0s9 -s $LNK -d $FWL1 --dport $WEB -j DNAT --to-destination $INT:$WEB
+#        iptables -t nat -A PREROUTING -p tcp -i enp0s9 -s $LNK -d $FWL1 --dport $WEB -j DNAT --to-destination $STG:$WEB
 #done 
 
 # 5 - Habilita acesso do clente Externo ao MTA
