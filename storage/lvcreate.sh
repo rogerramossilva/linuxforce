2ea7293171cb8ac2d17f804cf1090a5d93f8c908
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2023-01-21 14:56:24 -0300

    Update lvcreate.sh

--- storage/lvcreate.sh
+++ storage/lvcreate.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 for i in devel drivers infrastructure lixeiras manager marketing owner profiles public security; do lvcreate -L 2G -n $i storage; done
 lvcreate -L 25G -n homes storage
