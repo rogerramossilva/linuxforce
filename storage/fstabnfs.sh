6bd7985a563003d78a760bf398a1dbd0e7465d88
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2023-01-30 22:30:49 -0300

    Update fstabnfs.sh

--- storage/fstabnfs.sh
+++ storage/fstabnfs.sh
@@ -3,5 +3,5 @@
 for i in drivers infrastructure lixeiras manager marketing owner profiles public security devel homes; do 
 mkdir -pv /srv/asf/samba/$i; 
 echo "192.168.1.30:/srv/asf/samba/$i /srv/asf/samba/$i nfs defaults 0 0 " >> /etc/fstab ; 
-chmod +w /srv/asf/samba/$i
+chmod a+w /srv/asf/samba/$i
 done
