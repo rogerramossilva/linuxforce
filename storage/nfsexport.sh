fa6e144472ca2c04e81e497becbddefee019f50b
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2023-04-13 19:59:52 -0300

    Update nfsexport.sh

--- storage/nfsexport.sh
+++ storage/nfsexport.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in drivers infrastructure lixeiras manager marketing owner profiles public security devel homes; do echo -e "/srv/asf/samba/$i  \t\t192.168.1.0/24(rw,no_root_squash)" >> /etc/exports; done
+for i in drivers infrastructure lixeiras manager marketing owner profiles public security devel homes; do echo -e "/srv/asf/samba/$i  \t\t192.168.1.0/24(rw,no_root_squash,no_subtree_check)" >> /etc/exports; done
