0fb7f602fe4dc2911b893883811431f0ab666eb8
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2023-04-17 19:42:45 -0300

    Create userandgroups.sh

---
+++ samba/scripts/userandgroups.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Array com grupos de sistema
+all_groups=('infrastructure' 'security' 'public' 'manager' 'marketing' 'owner' 'devel')
+for group in ${all_groups[@]}; do
+	groupadd $group      
+done
+
+# Array com Users de sistema
+all_users=('gideon.goddard' 'elliot.alderson' 'lloyd.chung' 'angela.moss' 'ollie.parker'
+for users in ${all_users[@]}; do
+	useradd -m -k /etc/skel -d /srv/asf/samba/homes/$users -s /bin/bash  $users      
+done
